d0e0ab0a9f7b0d4a3bd9c47fad958f98cd104e79
praveenbabu36 <praveen.babu36@gmail.com>
2023-01-02 12:52:16 -0500

    Time the func calls

--- Week-3/ex-3-1/custom-memoize-add.js
+++ Week-3/ex-3-1/custom-memoize-add.js
@@ -3,10 +3,17 @@ function add(a, b) {
     return a+b;
 }
 
+// time the func call
+function time(fn){
+    console.time();
+    console.log(fn());
+    console.timeEnd();
+}
+
 // add-1
-console.log(add(100, 100));
-console.log(add(100, 100));
+time( () => add(100,100) );
+time( () => add(100,100) );
 
 // add-2
-console.log(add(200, 100));
-console.log(add(200, 100));
+time( () => add(200,100) );
+time( () => add(200,100) );
