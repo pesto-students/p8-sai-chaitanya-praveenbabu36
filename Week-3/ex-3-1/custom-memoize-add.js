78e96bc4d532a0b312477dad43f752ccf68f2bf6
praveenbabu36 <praveen.babu36@gmail.com>
2023-01-02 12:43:25 -0500

    Simple add method to be memoized

---
+++ Week-3/ex-3-1/custom-memoize-add.js
@@ -0,0 +1,12 @@
+//reducer
+function add(a, b) {
+    return a+b;
+}
+
+// add-1
+console.log(add(100, 100));
+console.log(add(100, 100));
+
+// add-2
+console.log(add(200, 100));
+console.log(add(200, 100));
