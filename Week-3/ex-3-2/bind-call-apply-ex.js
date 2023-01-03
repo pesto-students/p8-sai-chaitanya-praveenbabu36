25030cef42e6823fb37f35e1eb0a0ae7b7ad0368
praveenbabu36 <praveen.babu36@gmail.com>
2023-01-03 12:23:28 -0500

    example for bind

--- Week-3/ex-3-2/bind-call-apply-ex.js
+++ Week-3/ex-3-2/bind-call-apply-ex.js
@@ -9,4 +9,16 @@ var circleObj = {
     }
 }
 
+// call the func using the obj ref
 console.log(circleObj.getArea());
+
+// create a new ref to the func and call it
+var getCircleArea = circleObj.getArea;
+
+// did nt work bcoz the this lost the reference to the radius variable
+console.log(getCircleArea()); 
+
+//bind
+// use bind to attach circleObj to the this
+console.log(getCircleArea.bind(circleObj)()); 
+
