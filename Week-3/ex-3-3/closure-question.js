c856f365c37e71cf2187dda5a178d5c89bf52b15
praveenbabu36 <praveen.babu36@gmail.com>
2023-01-03 23:02:48 -0500

    Question about closures. Week 3 Ex 3

---
+++ Week-3/ex-3-3/closure-question.js
@@ -0,0 +1,32 @@
+function createIncrement() {
+
+    let count = 0;
+
+    function increment(){
+        count++;
+    }
+
+    let message = `Count is ${count}`;
+
+    function log() {
+        console.log(message);
+    }
+
+    return [increment, log];
+
+}
+
+
+const [increment, log] = createIncrement();
+increment();
+increment();
+increment();
+log(); // What is logged ?
+
+/* 
+
+Question : What is the output and Why ? 
+
+
+
+*/
