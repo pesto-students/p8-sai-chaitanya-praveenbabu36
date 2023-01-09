e0d1c5ffbb99784599585536b2d6caccc900ff8b
praveenbabu36 <praveen.babu36@gmail.com>
2023-01-09 13:23:35 -0500

    Correct implementation of stack using closure

--- Week-4/ex-4/stack-impl-closure.js
+++ Week-4/ex-4/stack-impl-closure.js
@@ -5,35 +5,33 @@
       no way to access items array outside of createStack() function scope.
 */
 function createStack() {
+  const items = [];
 
-    function setupStack() {
-        const items = [];
- 
-        function push(item){
-            return items.push(item);
-        }
+  return {
+    push: function (item) {
+      return items.push(item);
+    },
 
-        function pop(){
-            return items.pop();
-        }
+    pop: function () {
+      return items.pop();
+    },
 
-        function getItems(){
-            return items;
-        }
-
-        return {push, pop, getItems};
-    }
-
-    
-    return Object.preventExtensions(setupStack());
+    getItems: function () {
+      return items;
+    },
+  };
 }
 
-// create stack, add item and list 
+// create stack, add item and list
 const stack1 = createStack();
-stack1.push(100);
-stack1.push(500);
-stack1.push(1);
-stack1.pop();
+stack1.push(101);
+stack1.push(502);
+stack1.push(8);
+stack1.push(11);
+console.log("#1 :", stack1.getItems());
+console.log("#2 :", stack1.pop());
+// This doesnt affect the items created when we called createStack() due to closure
 stack1.items = [12, 23, 23];
-console.log('1:', stack1.items); // undefined
-console.log('2:', stack1.getItems().toString()); // 100, 500
+console.log("#3:", stack1.items); 
+console.log("#4 :", stack1.pop());
+console.log("#5:", stack1.getItems()); // 100, 500
