0cde03b68269e588360815df350f9a446288fef0
praveenbabu36 <praveen.babu36@gmail.com>
2023-01-11 19:53:31 -0500

    ex-4.2 Subclass with method to use parent properties

--- Week-4/exercise-4.2.js
+++ Week-4/exercise-4.2.js
@@ -6,9 +6,27 @@ Person.prototype.initialize = function (name, age) {
   this.age = age;
 };
 
+//Req: Create the class Teacher
+var Teacher = function () {};
+
+//Make teacher a child of person
+//Object.setPrototypeOf(Teacher, Person); // why this is not working? question
+Object.setPrototypeOf(Teacher.prototype, Person.prototype);
+
+//teach function in subclass
+Teacher.prototype.teach = function (subject) {
+  this.subject = subject;
+  return this.name + " is now teaching " + this.subject;
+};
 
 //new teacher obj
-var him = new Person();
+var him = new Teacher();
 him.initialize("Praveen", 42);
 
-console.log(him);
+console.log(him.teach("Inheritance"));
+
+//new teacher obj 2
+var him = new Teacher();
+him.initialize("Mohan", 24);
+
+console.log(him.teach("History"));
