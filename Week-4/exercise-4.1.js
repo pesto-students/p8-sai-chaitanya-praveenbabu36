25cac76489f835342ba29d575715d8f92c091f4e
praveenbabu36 <praveen.babu36@gmail.com>
2023-01-14 20:41:30 -0500

    Custom Promise Implementation.

--- Week-4/exercise-4.1.js
+++ Week-4/exercise-4.1.js
@@ -1,3 +1,36 @@
+//custom promise function
+class MyPromise {
+  //constructor
+  constructor(callbackFunction) {
+    this.state = "Pending";
+    this.value = null;
+
+    const resolve = (...args) => {
+      this.state = "Resolved";
+      this.value = args;
+      console.log(this.state, ...args, "😻");
+    };
+
+    const reject = (...args) => {
+      this.state = "Rejected";
+      this.value = args;
+      console.log(this.state, ...args, '😈');
+    };
+
+    // call the function passed to promise
+    callbackFunction(resolve, reject);
+  }
+
+  //handle promise result - then function 
+  then(onSuccess, onFailure) {
+    if (this.state == "Resolved") {
+      onSuccess(this.value);
+    } else {
+      onFailure(this.value);
+    }
+  }
+}
+
 // Function returns a random number
 const getNumber = () => {
   //get random number
@@ -6,11 +39,38 @@ const getNumber = () => {
   //divBy_5_Flag
   const div_by_5_Flag = (num != 0) & (num % 5 == 0);
 
-  return new Promise((resolve, reject) => {
-    if (div_by_5_Flag) resolve(num);
+  //create a new promise
+  return new MyPromise(function (resolve, reject) {
+    if (!div_by_5_Flag) {
+      return reject(num);
+    }
 
-    reject(num);
+    return resolve(num);
   });
 };
 
-console.log(getNumber());
+
+// Handle Promise result.
+getNumber().then(
+  //success
+  (x) => {
+    console.log("1- Success:", x);
+  },
+  //failure
+  (x) => {
+    console.log("1 - Failure:", x);
+  }
+);
+
+
+// Handle Promise result.
+getNumber().then(
+  //success
+  (x) => {
+    console.log("2 - Success:", x);
+  },
+  //failure
+  (x) => {
+    console.log("2 - Failure:", x);
+  }
+);
